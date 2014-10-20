c40e28d4fd2545b7f330faab730c9a10b132e0e8
Jack Davis <jxd231@bham.ac.uk>
2014-10-20 13:38:28 +0100

    Added Class

---
+++ GA/MinimisePool.py
@@ -0,0 +1,196 @@
+'''
+Minimise Pool
+
+Jack Davis
+
+20/10/14
+'''
+
+import os
+import DFT_input as DFTin
+import DFT_output as DFTout
+import DFT_submit as DFTsub
+
+from Select import tournamentSelect as select
+from Crossover import crossover as cross 
+from checkPool import checkPool as checkPool
+from CoM import CoM 
+
+class minPool:
+
+	def __init__(self,natoms,eleNames,eleMasses,n,stride,hpc,mpitasks):
+		
+		self.natoms = natoms
+		self.eleNames = eleNames
+		self.eleMasses = eleMasses
+		self.n = n
+		self.stride = stride
+		self.hpc = hpc
+		self.mpitasks = mpitasks
+
+		self.runCalc()
+
+	def runCalc(self):
+
+		self.findStruc()
+		self.getXYZ()
+		self.minimise()
+	
+	def findStruc(self):
+
+		self.strucNum = 0
+			
+		self.checkDB
+		self.lockDB
+		self.readPool()
+		self.unlockDB
+
+		for line in self.poolList:		
+			self.strucNum += 1
+			if "NotMinimised" in line:
+				break
+
+	def getXYZ(self):
+
+		self.xyzNum = ((self.strucNum-1)/self.stride) + 1
+
+		'''
+		Grab structure 
+		from pool.dat
+		and write .xyz.
+		'''
+
+		self.checkDB
+		self.lockDB
+		
+		self.initialXYZ = self.poolList[self.strucNum-2:self.strucNum+self.stride-2]
+		
+		self.unlockDB
+
+		with open(str(self.xyzNum)+".xyz","w") as xyzFile:
+			for line in self.initialXYZ:
+				xyzFile.write(line)
+
+	def minimise(self):
+
+		'''
+		Write Running flag 
+		to pool and start 
+		DFT calculation.
+		'''
+
+		self.checkDB
+		self.lockDB
+
+		# Write Running flag to pool.dat
+		self.readPool()
+		self.poolList[self.strucNum-1] = "Running\n"
+		self.writePool()
+
+		self.unlockDB
+
+		# Run DFT calc
+		self.vaspIN = DFTin.vasp_input(self.xyzNum)
+		run = DFTsub.submit(self.hpc,self.xyzNum,self.mpitasks)
+		self.vaspOUT = DFTout.vasp_output(self.xyzNum,self.natoms)
+
+		if self.vaspOUT.error:
+			print "*- Error in VASP Calculation -*"
+		else:
+			self.updatePool()
+
+	def updatePool(self):
+
+		'''
+		After completion take
+		final energy and coords
+		from OUTCAR and Update
+		poolList
+		'''
+
+		self.checkDB
+		self.lockDB
+
+		self.readPool()
+
+		box = 10.0
+
+		energy = self.vaspOUT.final_energy
+		finalXYZ = self.vaspOUT.final_coords
+		finalXYZele = self.finalCoords(self.initialXYZ[2:],finalXYZ,box)#self.vaspIN.box)
+		self.poolList[self.strucNum:self.strucNum+self.stride-2] = finalXYZele
+		self.poolList[self.strucNum-1] = "Finished Energy = " + str(energy) + "\n"
+		self.writePool()
+
+		self.unlockDB
+
+	def finalCoords(self,initialXYZ,finalXYZ,box):
+
+		'''
+		Adds element types 
+		to final coordinates
+		from OUTCAR. Removes 
+		from centre of box
+		'''
+
+		eleList = []
+		finalXYZele =[]
+
+		for line in initialXYZ:
+			ele, x,y,z = line.split()
+			eleList.append(ele)
+
+		# Take coords out of centre of box.
+		finalXYZ = [float(i) - box/2 for i in finalXYZ]
+	
+		# Convert list to str for writing to pool.
+		finalXYZ = [str(i) for i in finalXYZ]
+
+		for i in range(0,len(finalXYZ),3):
+			xyz = str(finalXYZ[i]) + " " \
+			+ str(finalXYZ[i+1]) + " " \
+			+ str(finalXYZ[i+2]) + "\n"
+			xyzLine = eleList[i/3] + " " + xyz
+			finalXYZele.append(xyzLine)
+
+		finalXYZele = CoM(finalXYZele,self.eleNames,self.eleMasses)
+
+		return finalXYZele
+
+	def readPool(self):
+
+		'''
+		Reads pool at beginning/
+		throughout calculation.
+		'''
+
+		with open("pool.dat","r") as pool:
+			self.poolList = pool.readlines()
+
+	def writePool(self):
+
+		'''
+		Writes pool to
+		file after any
+		changes.
+		'''
+
+		with open("pool.dat","w") as pool:
+			for line in self.poolList:
+				pool.write(line)
+
+	def lockDB(self):
+
+		with open("lock.db","w") as lock:
+			lock.write("locked")
+
+	def unlockDB(self):
+
+		os.system("rm lock.db")
+
+	def checkDB(self):
+
+		while os.path.exists("Lock.dat"):
+			pass 
+		else:
+			print "closed"
