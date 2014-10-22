98591767b7bd79a8725bdd112b8ce7e8cfc80e10
Jack Davis <jxd231@bham.ac.uk>
2014-10-22 15:03:31 +0100

    Running flag added

--- GA/Select.py
+++ GA/Select.py
@@ -15,7 +15,7 @@ class tournamentSelect:
 		self.n = n
 		self.energies = []
 		self.getEnergies()
-		self.select(n)
+		self.selectClusters(n)
 
 	def getEnergies(self):
 
@@ -30,8 +30,10 @@ class tournamentSelect:
 				if "Finished Energy" in line:
 					energyList = line.split()
 					self.energies.append(float(energyList[3]))
+				if "Running" in line:
+					self.energies.append(100.0)
 
-	def select(self,n):
+	def selectClusters(self,n):
 
 		'''
 		Selects random 
@@ -58,19 +60,32 @@ class tournamentSelect:
 
 		# Change to accept larger tournaments
 		size = 2 # Do not change!
-		tournament = []
+		tournList = []
 
+		# Select two random clusters for comparison.
 		for i in range(size):
-			tournament.append(randrange(1,self.n+1))
+			tournList.append(randrange(1,self.n+1))
 
-		while tournament[0] == tournament[1]:
-			tournament[1] = randrange(1,self.n+1)
+		# Ensure that the two clusters are different. 
+		while tournList[0] == tournList[1]:
+			tournList[1] = randrange(1,self.n+1)
 
-		tournEnergy = []
+		# Get energies of two chosen clusters.
+		self.tournEnergy = []
+		for i in tournList:
+			self.tournEnergy.append(self.energies[i-1])
 
-		for i in tournament:
-			tournEnergy.append(self.energies[i-1])
+		# Get index of lowest energy from tournament. 
+		lowest = self.tournEnergy.index(min(self.tournEnergy))
 
-		lowest = tournEnergy.index(min(tournEnergy))
+		return tournList[lowest]
 
-		return tournament[lowest]
+	# def checkRunning(self):
+
+	# 	for i in range(len(self.tournEnergy)):
+	# 		if self.tournEnergy[i] == "Running":
+	# 			print "Running"
+	# 			return True
+	# 			break
+
+	# 	return False
