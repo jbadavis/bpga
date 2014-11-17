21683f8359ec40ebf157a0c438a5087f10f0bce1
Jack Davis <jxd231@bham.ac.uk>
2014-11-17 10:51:54 +0000

    fixOverlap file added to repo

---
+++ GA/fixOverlap.py
@@ -0,0 +1,34 @@
+'''
+fixOverlap
+
+Jack Davis
+
+13/11/14
+'''
+
+import numpy as np
+
+def fixOverlap(coords):
+
+	overlaped = True
+
+	while overlaped:
+		for i in range(0,len(coords),3):
+			for j in range(0,len(coords),3):
+				x=coords[i]-coords[j]
+				y=coords[i+1]-coords[j+1]
+				z=coords[i+2]-coords[j+2]
+				r=(np.sqrt((x**2)+(y**2)+(z**2)))
+				if r < .9 and r != 0:
+					diff = .9 - r
+					coords[i] += diff
+					coords[i+1] += diff
+					coords[i+2] += diff
+					overlaped=True
+					break
+				else:
+					overlaped = False
+			if overlaped == True:
+				break	
+
+	return coords
