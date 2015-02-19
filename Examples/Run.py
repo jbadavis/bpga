506ec269d33166c2a4ff094e74fff61e36af7cd4
Jack Davis <jxd231@bham.ac.uk>
2015-02-19 13:36:00 +0000

    VASP output.dat

--- Examples/Run.py
+++ Examples/Run.py
@@ -21,7 +21,7 @@ eleNames = ["Pt","Rh"]
 eleMasses = In.masses(eleNames)
 natoms = sum(eleNums)
 
-subString = "aprun -n 24 vasp5.gamma"
+subString = "aprun -n 24 vasp5.gamma > output.dat"
 
 In.checkFiles()
 
@@ -40,5 +40,5 @@ then producing offspring.
 '''
 
 StartCalc = poolGA(natoms,r_ij,eleNums
-	,eleNames,eleMasses,mutate,npool
-	,cross,mutType,subString)
+    ,eleNames,eleMasses,mutate,npool
+    ,cross,mutType,subString)
