6d82f2e19755bc95f463afb3ca107422d725c888
Jack Davis <jxd231@bham.ac.uk>
2015-05-27 16:54:18 +0100

    Index check when not minimised fix

--- GA/checkPool.py
+++ GA/checkPool.py
@@ -27,8 +27,10 @@ class checkPool:
 				if "Finished Energy" in line:
 					energyList = line.split()
 					self.energies.append(float(energyList[3]))
+				elif "Running" in line:
+					self.energies.append(-1000000)
 					
-	def checkEnergy(self,newEnergy):
+ 	def checkEnergy(self,newEnergy):
 
 		HighestEnergy = max(self.energies)
 		self.lowestIndex = self.energies.index(HighestEnergy)
