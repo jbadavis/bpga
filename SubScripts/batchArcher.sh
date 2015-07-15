f9be88b9f52138c347eaeca51201ac0f258cea5e
Jack Davis <jxd231@bham.ac.uk>
2015-07-15 10:46:01 +0100

    Updated example

--- SubScripts/batchArcher.sh
+++ SubScripts/batchArcher.sh
@@ -1,23 +1,28 @@
 #!/bin/bash --login
 #PBS -r y
-#PBS -N GA
-#PBS -l select=2
-#PBS -l walltime=12:00:0
+#PBS -N BPGA
+#PBS -l select=8
+#PBS -l walltime=8:00:0
 #PBS -A e05-react-roy
 
 export PBS_O_WORKDIR=$(readlink -f $PBS_O_WORKDIR)
 cd $PBS_O_WORKDIR
+base=$PBS_O_WORKDIR
 
 module load vasp5
 
-# Stops job if program crashes
-# and begins running on the 
-# launcher nodes. 
-ulimit -t 3600
+# Specify the number of parallel instances.
+instances=4 
 
-for i in {1..2}; do
-  python Run.py &
-  sleep 10
+# For multiple compositions add directory 
+# names to first loop
+for i in Au2Rh2 ; do 
+  # Do not change! 
+  for (( j=1; j<=$instances; j++)); do 
+    echo $i $j
+    #python Run.py &
+    #sleep 10
+  done
 done
 
 wait
